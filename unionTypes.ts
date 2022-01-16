2e7619d674fe0f371590faba69949449599bbcec
Nandy <nandhirajanece@gmail.com>
2022-01-16 18:08:00 +0530

    Working with Union Types

---
+++ unionTypes.ts
@@ -0,0 +1,11 @@
+type playersArray = Array<string | number>;
+let players: playersArray = ["Nandhirajan", "Ramkrish"];
+let player_numbers: playersArray = [1, 2];
+console.log(players);
+
+var names: string | string[];
+names = "Nandhirajan";
+console.log(names);
+
+names = ["Nandhirajan", "Ramkrish"];
+console.log(names);
