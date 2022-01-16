65cf82fa6f0db620b905bf29affbdeca3f1d0d4f
Nandy <nandhirajanece@gmail.com>
2022-01-16 17:21:42 +0530

    Working with variables

---
+++ variables.js
@@ -0,0 +1,7 @@
+var fullName = "Nandhirajan";
+var paidAccount = true;
+var versionNumber = 1;
+console.log("Full Name - ", fullName);
+console.log("Paid Account - ", paidAccount);
+console.log("Version Number - ", versionNumber);
+//# sourceMappingURL=variables.js.map
---
+++ variables.ts
@@ -0,0 +1,7 @@
+var fullName: string = "Nandhirajan";
+let paidAccount: boolean = true;
+const versionNumber: number = 1;
+
+console.log("Full Name - ", fullName);
+console.log("Paid Account - ", paidAccount);
+console.log("Version Number - ", versionNumber);
